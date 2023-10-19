9b40251d5a4df6dab390aa6ebfc25681d228473d
AndreaReynoso132 <95582384+AndreaReynoso132@users.noreply.github.com>
2023-10-18 22:31:20 -0300

    Se agregan correciones sobre las imagenes de los productos

--- TecnicaturaJavaScript/E-commerce/client/js/product.js
+++ TecnicaturaJavaScript/E-commerce/client/js/product.js
@@ -4,48 +4,48 @@ const productos = [
         productName: "Samsung A04",
         price: 8000,
         quantity: 1,
-        img: "/client/media/a04_1_.png",
+        img: "../media/a04_1_.png",
     },
     {
         id: 2,
         productName: "Samsung A35",
         price: 7000,
         quantity: 1,
-        img: "/client/media/a34-5g_1.png",
+        img: "../media/a34-5g_1.png",
     },
     {
         id: 3,
         productName: "Motorola G52",
         price: 4000,
         quantity: 1,
-        img: "media/g52-negro-frente.jpg",
+        img: "../media/g52-negro-frente.png",
     },
     {
         id: 4,
         productName: "Samsung A04e",
         price: 15000,
         quantity: 1,
-        img: "media/sam.jpg",
+        img: "../media/sam.png",
     },
     {
         id: 5,
         productName: "Motorola E13",
         price: 7800,
         quantity: 1,
-        img: "media/moto-e13-natural-front.png",
+        img: "../media/moto-e13-natural-front.png",
     },
     {
         id: 6,
         productName: "Motorola G23",
         price: 5300,
         quantity: 1,
-        img: "media/moto-g23-azul-front.jpg"
+        img: "../media/moto-g23-azul-front.png"
     },
     {
         id: 7,
         productName: "Motorola G23",
         price: 5300,
         quantity: 1,
-        img: "media/s-l960.jpg"
+        img: "../media/moto-g23-azul-front.png"
     },
 ];
